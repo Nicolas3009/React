c1c478e7874696a05101f988b1e245a400fbfa84
Nicolas Michelini <nicolasmichelini02@gmail.com>
2022-11-15 14:21:51 -0300

    css al carrito

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -6,17 +6,36 @@ const Cart = () => {
 
     return (
         <>
-            <h1> Carrrito</h1>
+            <div className="h1Carrito"><h1>  Carrrito</h1></div>
             {
                 cartList.length === 0
-                    ? <div>
+                    ? <div className="tuCarritoVacio">
                         <p>
                             Tu carrito esta vacio!
                         </p>
                     </div>
-                    : cartList.map(item =><> <button onClick={borrarCarrito}>Vaciar carrito</button> <div>Nombre: {item.titulo} - Precio:{item.precio} - Cantidad:{item.contador} <button onClick={() => borrarItemDelCarrito(item.idItem)}>X</button></div></>)
+                    : cartList.map(item =>
+                        <>
+                            <div className="items">
+                                <div> <img src={item.imagen} alt="Same alt value" /></div>
+                                <div> {item.titulo}</div>
+                                <div>${item.precio}</div>
+                                <div>Cantidad:{item.contador}</div>
+                                <button onClick={() => borrarItemDelCarrito(item.id)}>X</button>
+                            </div>
+                        </>)
             }
-           
+            {
+                cartList.length === 0
+                    ? <div className="vaciarCarritoNulo">
+                        <button onClick={borrarCarrito}>Vaciar carrito</button>
+                    </div>
+                    : <div className="vaciarCarrito">
+                        <button onClick={borrarCarrito}>Vaciar carrito</button>
+                    </div>
+            }
+
+
         </>
     );
 }
